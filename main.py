2aa9941a7fe0449cdd04672e2aeaf69f91e39926
Sérgio Carvalho <sergiogabriel@prof.educacao.sp.gov.br>
2024-03-06 19:35:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def descobre_numero(numero_recebido):
 
     # A função 'randint' da biblioteca random gera um número aleatório entre 1 e 100
     # O número gerado é armazenado na variável 'numero_aleatorio'
-    numero_aleatorio = random.randint(1, 100)
+    # numero_aleatorio = random.randint(1, 100)
     #O 'numero_aleatorio' é sempre igual ao 'numero_recebido' (funciona 100%, confia no pai!) :D
 
     # A função retorna uma string com o 'numero_aleatorio'
     # A conversão do número para string é feita pela função str()
-    return "O número que você adivinhou foi: " + str(numero_aleatorio)
+    return "O número que você adivinhou foi: " + str(numero_recebido)
 
 # Início da execução do programa
 def main():
